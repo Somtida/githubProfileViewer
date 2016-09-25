d230ad0278716582da44ee8aea7268bb5c6dc603
S.SKNS <somtida.th@gmail.com>
2016-09-24 18:38:01 -0700

    add github

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      username: 'somtida',
+      userData: [],
+      userRepos: [],
+      perPage: 5
+    }
+  }
   render(){
     return(
       <div>
@@ -9,6 +18,15 @@ class App extends Component{
       </div>
     )
   }
+}
+
+App.propTypes = {
+  clientId: React.PropTypes.string,
+  clientSecret: React.PropTypes.stirng
+};
+App.defaultProps = {
+  clientId: '',
+  clientSecret: '',
 }
 
 export default App
